73265347000c37e5016b1a4f7fefbed057affc77
CatZhang <CrazyCatZhang@outlook.com>
2020-06-24 12:19:30 +0800

    MicroServiceSSO
    The basic implementation of SSO

---
+++ MicroServiceSSO/src/main/java/com/catzhang/microservicesso/MicroservicessoApplication.java
@@ -0,0 +1,13 @@
+package com.catzhang.microservicesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicessoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicessoApplication.class, args);
+    }
+
+}
---
+++ MicroServiceSSO/src/test/java/com/catzhang/microservicesso/MicroservicessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catzhang.microservicesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicessoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
